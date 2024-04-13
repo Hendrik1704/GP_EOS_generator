7781468d60fb5a13abcbd117ca4d8192799a778e
jacksongong <jacksongong2017@gmail.com>
2024-04-13 11:32:46 -0400

    Test Values Inserted into 1-D Model
    derivative test is under the def filter function. I also experimented and added a def percent_filter function which filters based on the percentage away from the y-mean function (generated from 1000 linspace points). I used the np.interp function to do this

--- EOS_Generator_1D.py
+++ EOS_Generator_1D.py
@@ -7,6 +7,12 @@ data=np.loadtxt("EoS_hotQCD.dat", dtype=float, skiprows=0)
 x_data=data[:,0].reshape(-1, 1) #the x value here must be reshaped so we only input a flat array
 
 y_data=data[:,1]
+
+data=np.loadtxt("EoS_hotQCD_full.dat", dtype=float, skiprows=0)
+x_test=data[:,0].reshape(-1, 1) #the x value here must be reshaped so we only input a flat array
+
+y_test=data[:,1]
+
 #must change so that data equals the number got from standard deviation
 noise=0.01
 
@@ -22,14 +28,30 @@ y_new=gpr.sample_y(x_simulate, 1, random_state=1000).flatten() #unlike ravel bec
 #create can index to relate numbers to one another
 slope=5
 def filter(x,y):
-    dydx=np.gradient(y, x.ravel())
+    dydx=np.gradient(y, x.ravel()) #creates a two dimensional array
     indices = np.where(dydx > slope)[0] #the [0] creates a new array of valid derivitives
     x_f = x[indices] #the valid elements will create Values_x
     y_f = y[indices]
     return (x_f,y_f)
-Values_x,Values_y=filter(x_simulate,y_new)
+Values_x,Values_y=filter(x_simulate,y_new) #these are filtering the 1000 randomly generated points
 y_mean, y_std = gpr.predict(Values_x, return_std=True) #this will return y_mean that is the size of the filtered 1000 point in that of Values_x
 x_finaldata,y_finaldata=filter(x_data,y_data)
+x_testdata,y_testdata=filter(x_test,y_test)
+#.flatten()
+p=50 #percent away from the y_mean curve
+shortened_y=np.interp(x_testdata,Values_y,y_mean) #instead of Values_x, we want to interpolate about Values_y; the values that are in the 1000 random point curve. Values_x will interpolate mean for y_test values
+def percent_filter(x,y): #we are multiplying 64 with a 657 value
+    indices = np.where(((1 - p/100) * shortened_y < y) & (y< (1 + p/100) * shortened_y))[0] #function creates a dictionary for bijections
+    x_tested = x[indices]
+    y_tested=y[indices]
+    if np.size(x_tested)<np.size(x):
+        return (x_tested,y_tested)
+    else:#this is so that the interp. function doesn't start giving values greater than the number of total test points
+        return(x_test,y_test)
+#print(shortened_y)
+#print("Lower bound example:", (1 - p/100) * shortened_y[:])
+#print("Upper bound example:", (1 + p/100) * shortened_y[:])
+x_passed,y_passed=percent_filter(x_testdata,y_testdata)
 
 
 plt.figure(figsize=(10, 5))
@@ -38,6 +60,10 @@ plt.ylabel("y--[P$T^{-4}$]")
 #what is the points of having Values_y?
 plt.plot(Values_x, y_mean, 'k', lw=1, ls='-', label=f'Mean Curve, Filter: dy/dx > {slope}')
 plt.plot(Values_x, Values_y, 'b', lw=1, ls='-', label='Derivative Graph: dy/dx')
+
+
+#plt.scatter(x_testdata, y_testdata, marker='x', color='orange', s=10, label=f'Test Data Received:{np.size(x_test)}, Test Data Remain: {np.size(x_testdata)}')#passes derivitive test
+plt.scatter(x_passed, y_passed, marker='x', color='orange', s=10, label=f'Test Data Received:{np.size(x_test)}, Test Data Passing Slope and Percent ({p}%) Test: {np.size(x_passed)}')#passes percent test
 
 plt.scatter(x_finaldata, y_finaldata, marker='x', color='r', s=10, label=f'Data Points Received:{np.size(x_data)},Data Points Remain: {np.size(x_finaldata)}')
 plt.errorbar(x_finaldata, y_finaldata, yerr=noise*y_finaldata, fmt='none',alpha=0.5, color='blue', label=f'{noise*100}% Error')
@@ -50,5 +76,5 @@ plt.fill_between(Values_x.flatten(), (1-noise)*y_mean-1*y_std, (1+noise)*y_mean+
 plt.fill_between(Values_x.flatten(), (1-noise)*y_mean-2*y_std, (1+noise)*y_mean+2*y_std, color='orange', label='95% confidence level',alpha=0.2)
 plt.fill_between(Values_x.flatten(), (1-noise)*y_mean-3*y_std, (1+noise)*y_mean+3*y_std, color='yellow', label='99.7% confidence level',alpha=0.2)
 
-plt.legend(title="Legend", loc='lower right', fontsize='small')
+plt.legend(title="Legend", loc='lower right', fontsize='x-small')
 plt.show()
