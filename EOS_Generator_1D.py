863c5b5598a148af58eea434a9888a41be800143
jacksongong <jacksongong2017@gmail.com>
2024-04-20 12:31:15 -0400

    4.20 updated code to filter curves instead of segments
    I have reduced both the correlation length and the number of points used in the linspace function. It seems like it takes lots of time to generate 1000 point with such low correlation length and strict slope requirements.

--- EOS_Generator_1D.py
+++ EOS_Generator_1D.py
@@ -1,80 +1,122 @@
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, WhiteKernel, CompoundKernel,RationalQuadratic, Matern, DotProduct,ConstantKernel
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel#, CompoundKernel,RationalQuadratic, Matern, DotProduct,ConstantKernel
 import numpy as np
 import matplotlib.pyplot as plt
 
 data=np.loadtxt("EoS_hotQCD.dat", dtype=float, skiprows=0)
 x_data=data[:,0].reshape(-1, 1) #the x value here must be reshaped so we only input a flat array
-
 y_data=data[:,1]
 
+
+# use test data only in the scatter plot: not in the actual filtering proccess
 data=np.loadtxt("EoS_hotQCD_full.dat", dtype=float, skiprows=0)
 x_test=data[:,0].reshape(-1, 1) #the x value here must be reshaped so we only input a flat array
-
 y_test=data[:,1]
 
-#must change so that data equals the number got from standard deviation
-noise=0.01
 
-kernel =WhiteKernel(noise_level=noise) + RBF(length_scale=0.1001483459) #this determains how far away we are from the actual curve (the uncertainty)
+#all customizability labeled here
+
+# noise/ uncertainity added throught the white kernel
+noise=0.01
+# slope requirements
+slope=0
+#percentage away from the mean
+p = 50
+randomness=np.random.seed(2)
+#number of random samples that we want
+number=2
+#changing number points generated in linspace random genrating function
+points=255
+i=0
+f=0
+
+
+#customize the kernel used: here, we have noise(uncertainty) with the white kernel)
+
+#you haven't done anything with the kernel here
+kernel =WhiteKernel(noise_level=noise) + RBF(length_scale=0.001818) #0.0001818 is the cutoff value until the curve oscillates without any fillters
 gpr = GaussianProcessRegressor(kernel=kernel, alpha=1e-2)
 gpr.fit(x_data, y_data)
+#y_mean, y_std = gpr.predict(Values_x, return_std=True)
 
-points=1000 #changing number points here in linspace
-x_simulate = np.linspace(0, 0.5, points).reshape(-1, 1)
-y_new=gpr.sample_y(x_simulate, 1, random_state=1000).flatten() #unlike ravel becuase ravel will NOT creat a copy---anyn changes made afefcts original;
+x_set=[]
+y_set=[]
 
-#take the numerical derivitive
-#create can index to relate numbers to one another
-slope=5
-def filter(x,y):
-    dydx=np.gradient(y, x.ravel()) #creates a two dimensional array
-    indices = np.where(dydx > slope)[0] #the [0] creates a new array of valid derivitives
-    x_f = x[indices] #the valid elements will create Values_x
-    y_f = y[indices]
-    return (x_f,y_f)
-Values_x,Values_y=filter(x_simulate,y_new) #these are filtering the 1000 randomly generated points
-y_mean, y_std = gpr.predict(Values_x, return_std=True) #this will return y_mean that is the size of the filtered 1000 point in that of Values_x
-x_finaldata,y_finaldata=filter(x_data,y_data)
-x_testdata,y_testdata=filter(x_test,y_test)
-#.flatten()
-p=50 #percent away from the y_mean curve
-shortened_y=np.interp(x_testdata,Values_y,y_mean) #instead of Values_x, we want to interpolate about Values_y; the values that are in the 1000 random point curve. Values_x will interpolate mean for y_test values
-def percent_filter(x,y): #we are multiplying 64 with a 657 value
-    indices = np.where(((1 - p/100) * shortened_y < y) & (y< (1 + p/100) * shortened_y))[0] #function creates a dictionary for bijections
-    x_tested = x[indices]
-    y_tested=y[indices]
-    if np.size(x_tested)<np.size(x):
-        return (x_tested,y_tested)
-    else:#this is so that the interp. function doesn't start giving values greater than the number of total test points
-        return(x_test,y_test)
-#print(shortened_y)
-#print("Lower bound example:", (1 - p/100) * shortened_y[:])
-#print("Upper bound example:", (1 + p/100) * shortened_y[:])
-x_passed,y_passed=percent_filter(x_testdata,y_testdata)
+size_list=[]
+sample_number= []
 
 
-plt.figure(figsize=(10, 5))
-plt.xlabel("x--[Temperature (GEV)]")
-plt.ylabel("y--[P$T^{-4}$]")
-#what is the points of having Values_y?
-plt.plot(Values_x, y_mean, 'k', lw=1, ls='-', label=f'Mean Curve, Filter: dy/dx > {slope}')
-plt.plot(Values_x, Values_y, 'b', lw=1, ls='-', label='Derivative Graph: dy/dx')
+
+#this for loop will try vales of randomly generated function until
+while i in range (0,number):
+    # create a while loop such that for each sample , we create a now set of data ponts, we must add numpy arrays to a pandas data frame
+    x_simulate = np.linspace(0, 0.5, points).reshape(-1, 1)
+    y_simulate = gpr.sample_y(x_simulate, 1, random_state=randomness).flatten()
+
+    def filter(x, y):
+        dydx = np.gradient(y, x.flatten())  # creates a two dimensional array
+        indices = np.where(dydx >= slope)[0]  # the [0] creates a new array of valid derivitives
+        x_f = x[indices]  # the valid elements will create x_f
+        # this is incorrect because we only return segments of the curve that work (we then only graph these segments, so a sin curve will have discrete display)
+        size=np.size(x_f)
+
+#this will print the values of desired (x) and x_f which is the trial
+        # print(len(x_f))
+        # print(len(x))
+
+        size_list.append(len(x_f))
+        sample_number.append(f+1)
+
+        if len(x_f)==len(x):
+            return True
+        else:
+            return False
+    boolean=filter(x_simulate, y_simulate)
+
+    if boolean==True:
+        x_set.append(x_simulate.flatten())
+        y_set.append(y_simulate)
+        print(f"\n\n\n\nWorking Pairs Set {i+1}:", end='\n')
+        for k in range(len(x_simulate)):
+            print(f"{x_simulate[k].flatten(), y_simulate[k]}",end='\n')
+
+        i=i+1
+    elif boolean==False:
+        i=i
+    #i is the number of samples that work
+
+#these values will actually show each trial
+    # print(size_list)
+    # print(sample_number)
 
 
-#plt.scatter(x_testdata, y_testdata, marker='x', color='orange', s=10, label=f'Test Data Received:{np.size(x_test)}, Test Data Remain: {np.size(x_testdata)}')#passes derivitive test
-plt.scatter(x_passed, y_passed, marker='x', color='orange', s=10, label=f'Test Data Received:{np.size(x_test)}, Test Data Passing Slope and Percent ({p}%) Test: {np.size(x_passed)}')#passes percent test
+    #f counts the total number of times the filter has run
+    f=f+1
 
-plt.scatter(x_finaldata, y_finaldata, marker='x', color='r', s=10, label=f'Data Points Received:{np.size(x_data)},Data Points Remain: {np.size(x_finaldata)}')
-plt.errorbar(x_finaldata, y_finaldata, yerr=noise*y_finaldata, fmt='none',alpha=0.5, color='blue', label=f'{noise*100}% Error')
 
-#note that ALL 3 curves will have 498 points remaining by the restriction of Values_x. change the slope for different points remaining
-i=np.random.seed(10) #this from the random library allows us to create ANYN set of n lines (we set n to be 3 for now)
-for t in range (3): #we can change the range value here to incoporate curves 0 to n-1, in this case n=3
-    plt.plot(Values_x,gpr.sample_y(Values_x, 1, random_state=i), lw=1, ls='--', label=f'Gaussian Curve {t+1}, Predicted Poins:{points}, Remaining: {np.size(Values_x)}')
-plt.fill_between(Values_x.flatten(), (1-noise)*y_mean-1*y_std, (1+noise)*y_mean+1*y_std, color='red', label='68% confidence level',alpha=0.2)
-plt.fill_between(Values_x.flatten(), (1-noise)*y_mean-2*y_std, (1+noise)*y_mean+2*y_std, color='orange', label='95% confidence level',alpha=0.2)
-plt.fill_between(Values_x.flatten(), (1-noise)*y_mean-3*y_std, (1+noise)*y_mean+3*y_std, color='yellow', label='99.7% confidence level',alpha=0.2)
+print(f"Filter repeats needed:{len(sample_number)}")
+plt.scatter(sample_number,size_list)
+plt.title(f"Graph of Each Trial, Trials Needed: {len(sample_number)}")
 
-plt.legend(title="Legend", loc='lower right', fontsize='x-small')
-plt.show()
+
+
+for i in range (len(x_set)):
+    plt.figure(figsize=(10, 5))
+    x_i=x_set[i].reshape(-1,1)
+    y_i=y_set[i]
+    y_mean, y_std = gpr.predict(x_i, return_std=True)  # this will return y_mean that is the size of the filtered points
+    plt.fill_between(x_i.flatten(), (1 - noise) * y_mean - 1 * y_std, (1 + noise) * y_mean + 1 * y_std, color='red', label='68% confidence level', alpha=0.2)
+    plt.fill_between(x_i.flatten(), (1 - noise) * y_mean - 2 * y_std, (1 + noise) * y_mean + 2 * y_std, color='orange', label='95% confidence level', alpha=0.2)
+    plt.fill_between(x_i.flatten(), (1 - noise) * y_mean - 3 * y_std, (1 + noise) * y_mean + 3 * y_std, color='yellow', label='99.7% confidence level', alpha=0.2)
+
+    plt.scatter(x_data,y_data, marker='x', color='r', s=10, label=f"Number Original Data Points: {len(x_data)}")
+    plt.errorbar(x_data, y_data, yerr=noise * y_data, fmt='none', alpha=0.5, color='blue', label=f'{noise * 100}% Error')
+    plt.scatter(x_test,y_test,marker='x', color='orange', s=10, label=f"Number Test Data Points: {len(x_test)}")
+    plt.plot(x_i,y_i, 'k', lw=1, ls='-', label=f'Filtered Curve {i+1}, Filter: dy/dx >= {slope}')
+    plt.title(f'Filtered Curve {i+1}')
+    plt.xlabel("x--[Temperature (GEV)]")
+    plt.ylabel("y--[P$T^{-4}$]")
+    plt.legend(title="Legend", loc='lower right', fontsize='x-small')
+    plt.show()
+
+
