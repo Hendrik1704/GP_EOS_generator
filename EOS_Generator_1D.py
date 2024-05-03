corrected the derivative computation

EOS_Generator_1D.py
@@ -4,9 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def compute_derivative(x, y):
+    """This function compute dy/dx using finite difference"""
+    dydx = np.zeros(len(y))
+    dydx[1:-1] = (y[2:] - y[:-2])/(x[2:] - x[:-2])
+    #dydx[1:] = (y[1:] - y[:-1])/(x[1:] - x[:-1])
+    dydx[0] = (y[1] - y[0])/(x[1] - x[0])
+    dydx[-1] = (y[-1] - y[-2])/(x[-1] - x[-2])
+    return(dydx)
+
+
 def compute_energy_density(T, P):
     """This function computes energy density"""
-    dPdT = np.gradient(P, T)
+    dPdT = compute_derivative(T, P)
     e = T * dPdT - P    # energy density
     return e
 
@@ -14,7 +24,7 @@ def compute_energy_density(T, P):
 def compute_speed_of_sound_square(T, P):
     """This function computes the speed of sound square"""
     e = compute_energy_density(T, P)
-    dPde = np.gradient(P, e)
+    dPde = compute_derivative(e, P)
     return dPde
 
 
@@ -23,7 +33,7 @@ def derivative_filter(x, y) -> bool:
         This filter check whether the derivative is larger than 0
         for all array elements
     """
-    dydx = np.gradient(y, x)
+    dydx = compute_derivative(x, y)
     indices = dydx < 0.
     negative_derivatives = x[indices]
 
@@ -46,13 +56,12 @@ def speed_sound_squared_filter(T, P):
 
 
 def is_a_physical_eos(T, P) -> bool:
-    return True
     if not derivative_filter(T, P):
         return False
 
-    dPdT = np.gradient(P, T)
-    if not derivative_filter(T, dPdT):
-        return False
+    #dPdT = compute_derivative(P, T)
+    #if not derivative_filter(T, dPdT):
+    #    return False
 
     if not speed_sound_squared_filter(T, P):
         return False
@@ -74,7 +83,7 @@ def main():
     validation_data = np.loadtxt("EoS_hotQCD_full.dat")
 
     # set the random seed
-    randomness = np.random.seed(23)
+    randomness = np.random.seed(19)
 
     number_of_EoS = 10
 
@@ -92,14 +101,19 @@ def main():
 
     EOS_set = []
 
-    i = 0
-    while i < number_of_EoS:
+    iSuccess = 0
+    iter = 1
+    while iSuccess < number_of_EoS:
+        if iter % 100 == 0:
+            print(f"Sample success rate: {float(iSuccess)/iter}")
         log_PoverT4_GP = gpr.sample_y(log_T_GP, 1,
                                       random_state=randomness).flatten()
         P_GP = np.exp(log_PoverT4_GP)*(T_GP**4)       # convert to P
         if is_a_physical_eos(T_GP, P_GP):
             EOS_set.append(P_GP)
-            i += 1
+            iSuccess += 1
+        iter += 1
+    print(f"Sample success rate: {float(iSuccess)/iter:.3f}")
 
     # make verification plots
     plt.figure()
@@ -113,6 +127,30 @@ def main():
     plt.ylim([0, 4.5])
     plt.xlabel(r"T (GeV)")
     plt.ylabel(r"$P/T^{4}$")
+    plt.show()
+
+    # plot e vs T
+    plt.figure()
+    for i in range(number_of_EoS):
+        e = compute_energy_density(T_GP, EOS_set[i])
+        plt.plot(T_GP, e/T_GP**4, '-')
+
+    plt.xlim([0, 0.5])
+    plt.ylim([0, 15])
+    plt.xlabel(r"T (GeV)")
+    plt.ylabel(r"$e/T^4$")
+    plt.show()
+
+    # plot cs^2 vs T
+    plt.figure()
+    for i in range(number_of_EoS):
+        cs2 = compute_speed_of_sound_square(T_GP, EOS_set[i])
+        plt.plot(T_GP, cs2, '-')
+
+    plt.xlim([0, 0.5])
+    plt.ylim([0, 1.0])
+    plt.xlabel(r"T (GeV)")
+    plt.ylabel(r"$c_s^2$")
     plt.show()
 
 