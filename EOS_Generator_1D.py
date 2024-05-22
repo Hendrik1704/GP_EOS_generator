b4db78f7d3d28782efbff389bb79eea0c8c83c6a
jacksongong <166082474+jacksongong@users.noreply.github.com>
2024-05-21 21:22:30 -0400

    Refined changes 5/21, fixed some errors in graphing, and added some effeciency in the contolling of aprameters.

--- EOS_Generator_1D.py
+++ EOS_Generator_1D.py
@@ -2,71 +2,69 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel
 import numpy as np
 import matplotlib.pyplot as plt
-##
+
+#this compute_derivitive does the same thing as the gradient function
+def compute_derivative(x, y):
+    """This function compute dy/dx using finite difference"""
+    dydx = np.zeros(len(y))
+    dydx[1:-1] = (y[2:] - y[:-2])/(x[2:] - x[:-2])
+    dydx[0] = (y[1] - y[0])/(x[1] - x[0])
+    dydx[-1] = (y[-1] - y[-2])/(x[-1] - x[-2])
+    return(dydx)
 def derivitive_1_filter(x, y, slope):
     dydx = np.gradient(y, x)
     indices = dydx > slope
-    after_derivitive_filter = x[indices]
+    after_derivitive_filter = dydx[indices]
     if len(after_derivitive_filter) == len(x):
         return True
     else:
         return False
 
-def derivitive_1_append_list(x, y, slope,working_simulated_1_derivitive):
+def derivitive_1_append_list(x, y, slope,list):
     dydx = np.gradient(y, x)
     indices = dydx > slope
-    after_derivitive_filter = x[indices]
-
-    working_simulated_1_derivitive.append(len(after_derivitive_filter))
-
-    print(f"Achieved Value: {len(after_derivitive_filter)}")
+    after_derivitive_filter = dydx[indices]
     print(f"Required Derivitive 1:{len(x)}")
-    if len(after_derivitive_filter) == len(x):
-        return True
-    else:
-        return False
+    print(f"Achieved Value: {len(after_derivitive_filter)}")
+    list.append(len(after_derivitive_filter))
 
 def derivitive_2_filter(x, y, slope):
     dydx = np.gradient(y, x)
     indices = dydx > slope
-    after_derivitive_filter = x[indices]
+    after_derivitive_filter = dydx[indices]
     if len(after_derivitive_filter) == len(x):
         return True
     else:
         return False
 
-def derivitive_2_append_list(x, y, slope,working_simulated_2_derivitive):
+def derivitive_2_append_list(x, y, slope,list):
     dydx = np.gradient(y, x)
     indices = dydx > slope
-    after_derivitive_filter = x[indices]
-    print(f"(Achieved Value: {len(after_derivitive_filter)}")
+    after_derivitive_filter = dydx[indices]
     print(f"Required Derivitive 2:{len(x)}")
-    working_simulated_2_derivitive.append(len(after_derivitive_filter))
-    if len(after_derivitive_filter) == len(x):
-        return True
-    else:
-        return False
+    print(f"Achieved Value: {len(after_derivitive_filter)}")
+    list.append(len(after_derivitive_filter))
 
 def speed_sound_squared_filter(T, P):
     dPdT = np.gradient(P,T)
     e = T * dPdT - P
     dPdE = np.gradient(P,e)
     index = (0 < dPdE) & (dPdE < 1)
-    after_sound_filter = T[index]
+    after_sound_filter = dPdE[index]
     if len(dPdE) == len(after_sound_filter):
         return True
     else:
         return False
 
-def speed_sound_squared_append_list(P,T,working_simulated_sound):
+def speed_sound_squared_append_list(T,P,list):
     dPdT = np.gradient(P, T)
     e = T * dPdT - P
     dPdE = np.gradient(P, e)
     index = (0 < dPdE) & (dPdE < 1)
-    after_sound_filter = T[index]
+    after_sound_filter = dPdE[index]
     print(f"Required Sound Filter (numerical):{len(dPdE)}")
-    print(f"Achieved Value: {len(after_sound_filter)}\n------------------------------------------------------")
-    working_simulated_sound.append(len(after_sound_filter))
+    print(f"Achieved Value: {len(after_sound_filter)}")
+    list.append(len(after_sound_filter))
 
 
 def compute_speed_of_sound_square (T,P):
@@ -76,12 +74,12 @@ def compute_speed_of_sound_square (T,P):
     return c_squared
 
 def compute_value_of_et4 (T,P):
-    dPdT = np.gradient(P,T)  # creates dPdT for the simulated points--- firstly define deritive for entropy like before; dp/dt
-    e = T * dPdT - P  # +u_b * n_b+u_q * n_q+u_s * n_s  #these values can be ignored
+    dPdT = np.gradient(P,T)
+    e = T * dPdT - P
     desired_y_value=e/(T**4)
     return desired_y_value
 
-def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
+def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials,sliced_amount):
     data=np.loadtxt("EoS_hotQCD.dat", dtype=float, skiprows=0)
     x_data=data[:,0].reshape(-1, 1)
     y_data=data[:,1]
@@ -122,7 +120,7 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
     min_data = np.min(x_data)
     max_data = np.max(x_data)
     print(f"Minimum of the datapoints is: {min_data}")
-    print(f"Maximum of the datapoints is: {max_data}")
+    print(f"Maximum of the datapoints is: {max_data}\n-------------------------")
 
     pressure_original_data=x_data.flatten()**4*y_data
     sound_squared_orginial_dataset=compute_speed_of_sound_square(x_data.flatten(),pressure_original_data)
@@ -148,15 +146,15 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
         speed_sound_squared_append_list(x_simulate.flatten(), y_simulate, working_simulated_sound)
 
 
-        print(boolean_1_derivitive,boolean_2_derivitive,boolean_speedsound)
+        print(f"{boolean_1_derivitive,boolean_2_derivitive,boolean_speedsound}")
 
         if boolean_1_derivitive==True and boolean_2_derivitive==True and boolean_speedsound==True:
-            x_working_set.append(x_simulate.flatten())
-            y_working_set.append(y_simulate)
-            print(f"\n\n\n\nWorking Pairs Set {i+1}:\n\n", end='\n')
-            for k in range(len(x_simulate)):
-                flattened_x=x_simulate.ravel()
-                print(f"{x_simulate[k][0], y_simulate[k]}",end='\n')
+            x_working_set.append(x_simulate[sliced_amount:-sliced_amount].flatten())
+            y_working_set.append(y_simulate[sliced_amount:-sliced_amount])
+            print(f"\nWorking Pairs Set {i+1} Above:\n---------------------------------------", end='\n')
+            # for k in range(len(x_simulate)):
+            #     flattened_x=x_simulate.ravel()
+            #     #print(f"{x_simulate[k][0], y_simulate[k]}",end='\n')
 
             min_sim = np.min(y_simulate)
             max_sim = np.max(y_simulate)
@@ -168,9 +166,9 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
             # please note that  working_computed_speed_sound and working_simulated_sound are differant-
             # one serves purpose of scatter plot to show ALL values of sound chart in each trial,the other for the simulated values where all other booleans are also true
             c_squared = compute_speed_of_sound_square(x_simulate.flatten(), y_simulate)
-            working_computed_speed_sound.append(c_squared)
+            working_computed_speed_sound.append(c_squared[sliced_amount:-sliced_amount])
             computed_e_divide_T4=compute_value_of_et4(x_simulate.flatten(), y_simulate)
-            working_computed_e.append(computed_e_divide_T4)
+            working_computed_e.append(computed_e_divide_T4[sliced_amount:-sliced_amount])
 
             i=i+1
 
@@ -187,7 +185,7 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
         print(f"Min y Simulated Point [P] for Case {t+1}: {min_simulated_pressure[t]}")
         print(f"Max y Simulated Point [P] for Case: {t + 1}: {max_simulated_pressure[t]}")
 
-    plt.scatter(number_iterations_while,working_simulated_1_derivitive)
+    plt.scatter(number_iterations_while,working_simulated_1_derivitive, label='Number of Satisfied Trials per loop')
     plt.xlabel('Filter Number')
     plt.ylabel('Equivalent Matching Points')
     plt.title(f"Graph of 1st Derivitive of Each Trial, Trials Needed: {working_trial_number[-1]}")
@@ -200,7 +198,7 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
     plt.legend(title="Legend", loc='upper left', fontsize='x-small')
     plt.show()
 
-    plt.scatter(number_iterations_while,working_simulated_2_derivitive)
+    plt.scatter(number_iterations_while,working_simulated_2_derivitive, label='Number of Satisfied Trials per loop')
     plt.xlabel('Filter Number')
     plt.ylabel('Equivalent Matching Points')
     plt.title(f"Graph of 2nd Derivitive of Each Trial, Trials Needed: {working_trial_number[-1]}")
@@ -213,7 +211,7 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
     plt.legend(title="Legend", loc='upper left', fontsize='x-small')
     plt.show()
 
-    plt.scatter(number_iterations_while,working_computed_speed_sound)
+    plt.scatter(number_iterations_while,working_simulated_sound, label='Number of Satisfied Trials per loop')
     plt.xlabel('Filter Number')
     plt.ylabel('Equivalent Matching Points')
     plt.title(f"Graph of Sound Value of Each Trial, Trials Needed: {working_trial_number[-1]}")
@@ -228,12 +226,13 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
 
     plt.figure(figsize=(10, 5))
 
+
     for i in range (len(x_working_set)):
         x_i=x_working_set[i].reshape(-1,1)
         y_i=y_working_set[i]
         plt.plot(x_i.flatten(),y_i/(x_i.flatten()**4), lw=1, ls='-', label=f'Filtered Curve {i+1}') #this will give the value for p/t^4
-    plt.scatter(x_data, y_data, marker='x', color='r', s=10, label=f"Number Original Data Points: {len(x_data)}")
-    plt.scatter(x_test, y_test, marker='x', color='orange', s=10, label=f"Number Test Data Points: {len(x_test)}")
+    plt.scatter(x_data[sliced_amount:-sliced_amount], y_data[sliced_amount:-sliced_amount], marker='x', color='r', s=10, label=f"Number Original Data Points: {len(x_data[sliced_amount:-sliced_amount])}")
+    plt.scatter(x_test[sliced_amount:-sliced_amount], y_test[sliced_amount:-sliced_amount], marker='x', color='orange', s=10, label=f"Number Test Data Points: {len(x_test[sliced_amount:-sliced_amount])}")
     plt.title(f'Filtered Gaussian Predict Curve, Filter: dy/dx > {slope}, d^2y/dx^2 > 2nd derivitive, sound squared restraint')
     plt.xlabel("x--[Temperature (GEV)]")
     plt.ylabel("y--[P$T^{-4}$]")
@@ -246,11 +245,10 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
     plt.title(f'Speed of Sound Curve')
     plt.xlabel("x--[Temperature (GEV)]")
     plt.ylabel("y--[Speed of Sound Squared c^2]")
-    plt.scatter(x_data, pressure_original_data, marker='x', color='r', s=10, label=f"Original Dataset Sound Squared, Number Data: {len(x_data)}")
-    plt.scatter(x_test, pressure_test_data, marker='x', color='orange', s=10, label=f"Test Dataset Sound Squared, Number Data: {len(x_test)}")
-    plt.legend(title="Legend", loc='upper left', fontsize='x-small')
+    plt.scatter(x_data[sliced_amount:-sliced_amount], sound_squared_orginial_dataset[sliced_amount:-sliced_amount], marker='x', color='r', s=10, label=f"Original Dataset Sound Squared, Number Data: {len(x_data[sliced_amount:-sliced_amount])}")
+    plt.scatter(x_test[sliced_amount:-sliced_amount], sound_squared_test_dataset[sliced_amount:-sliced_amount], marker='x', color='orange', s=10, label=f"Test Dataset Sound Squared, Number Data: {len(x_test[sliced_amount:-sliced_amount])}")
+    plt.legend(title="Legend", loc='lower right', fontsize='x-small')
     plt.show()
-
 
     for i in range(len(x_working_set)):
         x_i = x_working_set[i].reshape(-1, 1)
@@ -258,13 +256,17 @@ def main(ranSeed, slope,linspace_simulated_points,number_of_successful_trials):
     plt.title(f'Energy Density Curve')
     plt.xlabel("x--[Temperature (GEV)]")
     plt.ylabel("y--[e/T^4]")
-    plt.scatter(x_data, train_energy_density_set, marker='x', color='r', s=10, label=f"Original Dataset Energy Density, Number Data: {len(x_data)}")
-    plt.scatter(x_test, test_energy_density_set, marker='x', color='orange', s=10, label=f"Test Dataset Energy Density, Number Data: {len(x_test)}")
-    plt.legend(title="Legend", loc='upper left', fontsize='x-small')
+    #here, we use slices to get rid of extraneous values/outliers that create the verticle lines at the end of the graph
+    plt.scatter(x_data[sliced_amount:-sliced_amount], train_energy_density_set[sliced_amount:-sliced_amount], marker='x', color='r', s=10, label=f"Original Dataset Energy Density, Number Data: {len(x_data[sliced_amount:-sliced_amount])}")
+    plt.scatter(x_test[sliced_amount:-sliced_amount], test_energy_density_set[sliced_amount:-sliced_amount], marker='x', color='orange', s=10, label=f"Test Dataset Energy Density, Number Data: {len(x_test[sliced_amount:-sliced_amount])}")
+    plt.legend(title="Legend", loc='lower right', fontsize='x-small')
     plt.show()
+
 if __name__ == "__main__":
+    #by setting a main function, all the contollable variables can be set here.
     ranSeed = 23
     slope=0
     linspace_simulated_points = 1000
     number_of_successful_trials = 10
-    main(ranSeed,slope, linspace_simulated_points, number_of_successful_trials)
+    sliced_amount=5
+    main(ranSeed,slope, linspace_simulated_points, number_of_successful_trials,sliced_amount)
